39c0bb831c0f97da79d19d84b27303cdecfa9a7a
Thomas Adamson <taadamson33@gmail.com>
2023-08-28 18:02:34 -0400

    grid can be set by clicking cells; added randomize button to set cells automatically

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -4,21 +4,36 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-grid',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './grid.component.html',
   styleUrls: ['./grid.component.css']
 })
 export class GridComponent {
-  grid = [...new Array(10).fill(new Array(10).fill(0))]
 
-  constructor(){
-    console.log(this.grid);
+  grid = new Array();
+
+
+  ngOnInit(): void{
+    for(let i = 0; i < 10; i++){
+      this.grid.push([0,0,0,0,0,0,0,0,0,0])
+    }
   }
 
-  alterCell(cell: number): number{
-    cell ? cell = 0 : cell = 1;
-    console.log(cell)
-    return cell;
+  alterCell(i: number,j: number){
+    this.grid[i][j] ? this.grid[i][j] = 0 : this.grid[i][j] = 1;
   }
 
+  randomize(): void{
+    for(let i = 0; i < 10; i++){
+      for(let j = 0; j < 10; j++){
+        this.grid[i][j] = Math.floor(Math.random() * Math.floor(2));
+      }
+    }
+  }
+
+  startGame(): void{
+
+  }
 }
