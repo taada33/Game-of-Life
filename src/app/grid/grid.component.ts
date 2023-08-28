1ce52e3efa3cfb24740fd12c670dd9bafe21c3b1
Thomas Adamson <taadamson33@gmail.com>
2023-08-28 19:42:31 -0400

    grid evolution implemented

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -35,5 +35,71 @@ export class GridComponent {
 
   startGame(): void{
 
+    const nextGrid = new Array();
+
+    for(let i = 0; i < 10; i++){
+      nextGrid.push([0,0,0,0,0,0,0,0,0,0])
+    }
+
+    // let gridTotal=0;
+
+    // for(let i=0; i< this.grid.length;i++){
+    //   for(let j=0; j< this.grid.length;j++){
+    //     gridTotal+=this.grid[i][j];
+    //   }
+    // }
+
+    // while(gridTotal > 0){
+      for(let i = 0; i < this.grid.length; i++){
+        for(let j = 0; j < this.grid[i].length; j++){
+          let total = 0;
+          let lastCol = j-1;
+          let nextCol = j+1;
+          let lastRow = i-1;
+          let nextRow = i+1;
+  
+          if(lastCol < 0){
+            lastCol = this.grid.length-1;
+          }
+  
+          if(lastRow < 0){
+            lastRow = this.grid.length-1;
+          }
+  
+          if(nextCol > this.grid.length-1){
+            nextCol = 0;
+          }
+  
+          if(nextRow > this.grid.length-1){
+            nextRow = 0;
+          }
+
+          console.log(`lastRow = ${lastRow}, lastCol = ${lastCol} nextRow = ${nextRow}, nextCol = ${nextCol}`)
+  
+          total+= this.grid[lastRow][j];
+          total+= this.grid[lastRow][lastCol];
+          total+= this.grid[i][lastCol];
+          total+= this.grid[lastRow][nextCol];
+          total+= this.grid[nextRow][lastCol];
+          total+= this.grid[nextRow][nextCol];
+          total+= this.grid[i][nextCol];
+          total+= this.grid[nextRow][j];
+  
+          if(total === 2 || total === 3){
+            nextGrid[i][j] = 1;
+          }else{
+            nextGrid[i][j] = 0;
+          }
+        }
+      }
+
+      this.grid = [...nextGrid];
+
+    //   for(let i=0; i< this.grid.length;i++){
+    //     for(let j=0; j< this.grid.length;j++){
+    //       gridTotal+=this.grid[i][j];
+    //     }
+    //   }
+    // }
   }
 }
